c390fc97b88c6e79cd7c9209b94d1dd5230ea72a
Peter Snider <97427011+PeterTSnider@users.noreply.github.com>
2022-08-22 00:33:29 -0700

    projects page

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import GlobalStyles from "./assets/global";
 import Landing from "./pages/Landing";
-import Creds from "./pages/Creds"
+import Creds from "./pages/Creds";
 import About from "./pages/About";
+import Projects from "./pages/Projects";
 
 const App: React.FC = () => {
   return (
@@ -11,8 +12,9 @@ const App: React.FC = () => {
       <GlobalStyles />
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/creds" element={<Creds/>} />
-        <Route path="/about" element={<About/>} />
+        <Route path="/creds" element={<Creds />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
     </Router>
   );
